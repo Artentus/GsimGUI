9de7960bf495f995fe9d4299187a26e8f5bb964f
Mathis Rech <mathisrech@gmx.de>
2022-05-21 20:48:11 +0200

    Simplify enum bindings

--- EnumBindingSource.cs
+++ EnumBindingSource.cs
@@ -1,49 +1,20 @@
-﻿using Avalonia;
-using System;
+﻿using System;
 
 namespace GsimGUI
 {
-    internal class EnumBindingSource : AvaloniaObject
+    internal class EnumBindingSource : MarkupExtension<Array>
     {
-        private Type? _enumType;
-
-        public Type? EnumType
-        {
-            get => _enumType;
-            set
-            {
-                if (value != _enumType)
-                {
-                    if (value is not null)
-                    {
-                        Type enumType = Nullable.GetUnderlyingType(value) ?? value;
-                        if (!enumType.IsEnum) throw new ArgumentException("Type must be an Enum.");
-                    }
-
-                    _enumType = value;
-                }
-            }
-        }
-
-        public EnumBindingSource()
-        { }
+        public Type EnumType { get; }
 
         public EnumBindingSource(Type enumType)
         {
+            if (enumType is null) throw new ArgumentNullException(nameof(enumType));
+            if (!enumType.IsEnum) throw new ArgumentException("Type must be an Enum.");
+
             EnumType = enumType;
         }
 
-        public Array ProvideValue(IServiceProvider serviceProvider)
-        {
-            if (_enumType is null) throw new InvalidOperationException("EnumType must be specified.");
-
-            Type actualEnumType = Nullable.GetUnderlyingType(_enumType) ?? _enumType;
-            Array enumValues = Enum.GetValues(actualEnumType);
-            if (actualEnumType == _enumType) return enumValues;
-
-            Array tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
-            enumValues.CopyTo(tempArray, 1);
-            return tempArray;
-        }
+        public override Array ProvideValue(IServiceProvider serviceProvider)
+            => Enum.GetValues(EnumType);
     }
 }
---
+++ MarkupExtension.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using System;
+
+namespace GsimGUI
+{
+    internal interface IMarkupExtension
+    {
+        object? ProvideValue(IServiceProvider serviceProvider);
+    }
+
+    internal abstract class MarkupExtension<T> : AvaloniaObject, IMarkupExtension
+    {
+        public abstract T ProvideValue(IServiceProvider serviceProvider);
+
+        object? IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+            => ProvideValue(serviceProvider);
+    }
+}
